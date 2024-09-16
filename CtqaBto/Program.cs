d103ad41919a032d6b7bfe1d57ec7bb870e43308
tema5002 <152618947+tema5002@users.noreply.github.com>
2024-09-16 08:50:49 +0300

    Update Program.cs
    ok BRUMBLER statue building

--- CtqaBto/Program.cs
+++ CtqaBto/Program.cs
@@ -287,9 +287,18 @@ internal static class Data {
         "#minecraftphysics",
         "download rech2020 games for FREE",
         "CtqaLink",
-        "29A:AA79//@A>@4-->.4>"
+        "29A:AA79//@A>@4-->.4>",
+        "🌶️ копуста 🥐 🥧 🥧 перчик 🤯 🤯 перчик 🥧 перчик 🌶️ 🥐 🥐 🤯 🥐 пирчик копуста 🤯 киписти перчик киписти киписти копуста копуста копуста 🥐 копуста перчик 🌶️ копуста 🥧 пирчик перчик копуста копуста киписти перчик киписти копуста перчик 🥐 🥧 перчик пирчик перчик 🌶️ 🤯 🥧 🤯 🤯 🥧 киписти копуста киписти 🤯 пирчик 🥧 🤯 копуста перчик 🌶️ копуста киписти копуста 🤯 пирчик 🥐 пирчик перчик перчик пирчик пирчик перчик 🥧 🌶️ 🌶️ 🥧 🥧 копуста 🌶️ пирчик пирчик 🥧 киписти 🥧 киписти 🌶️ 🌶️ 🥐 киписти 🌶️ пирчик 🤯 🌶️ киписти пирчик 🥐 киписти 🌶️",
+        "untitled goose game 😔🎯",
+    "icosAHERONOOOOOOOOOOOOOOOO.ICOSAHEDROOOOOOOOOOOOOOOOD.S..ICIOCDHAOIDNEFP2O43[32[3PVMFDOVPFDMVNNM",
+        "why did mister breasts go to new zealand? because of electric caterpillars! get it? because i am kreisi",
+        "Me when i see the kreisi bruglar making oickles,: 😁😁😁📥🍔🍔😑🦣🦣😆😭🕷️🪞🥒🥐🥒🥒📚🦑",
+        "balls to the store 🏪😁balls to the store 🏪😁balls to the store 🏪😁balls to the store 🏪😁balls to the store 🏪😁balls to the store 🏪😁balls to the store 🏪😁",
+        "kreisi burglar alarm clock ⌚  ⏰",
+        "*kreisi music plays*",
+        "#TeamPicardias for life"
     ];
-    public static readonly string Datamine = "ctqa!ΔπβΔ©🐙αλ1Σhh1π1π©🐙Σ1π©βπΔΔ1βππhαββπλβππ🐙ΔhhαΔΔΣ1π🐙βλhαπβ©βββ1πΣβ🐙πΔβΣΔ🐙©αλαh🐙hΣβπh©ΣΔΔ🐙πλΣλλ11λhα🐙Δh©β©©πΔ©ΣβhΔλ🐙πΔβΔΔ🐙©ΣβββλαΔΣπ";
+    public static readonly string Datamine = "ctqa!ΔπβΔ©🐙αλ1Σhh1π1π©🐙Σ1π©βπΔΔ1βππhαββπλβππ🐙ΔhhαΔΔΣ1π🐙βλhαπβ©βββ1πΣβ🐙πΔβΣΔ🐙©αλαh🐙hΣβπh©ΣΔΔ🐙πλΣλλ11λhα🐙"Δh©β©©πΔ©ΣβhΔλ🐙πΔβΔΔ🐙©ΣβββλαΔΣπ";
     public static readonly string TrophyUnlocked = "<:ctqa_trophy:1200918336444309524>";
     public static readonly string TrophyLocked = "<:no_ctqa_trophy:1200918339938156554>";
 }
